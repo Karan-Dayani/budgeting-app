5a083d670c184f952676096b9f0ac27eef012c67
Karan <karandayani39@gmail.com>
2024-06-22 14:55:12 +0530

    date work

--- app/(tabs)/Expenses.jsx
+++ app/(tabs)/Expenses.jsx
@@ -1,15 +1,35 @@
-import { View, Text, ScrollView, SafeAreaView, Pressable, Modal, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Pressable,
+  Modal,
+  TextInput,
+} from "react-native";
 import React, { useState } from "react";
 import { Stack } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-import { Picker } from '@react-native-picker/picker';
-
+import { Picker } from "@react-native-picker/picker";
 
 export default function ExpensesPage() {
-  const [selectedDate, setSelectedDate] = useState("June 2024");
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toLocaleString("default", { month: "long", year: "numeric" })
+  );
   const [modalVisible, setModalVisible] = useState(false);
-  const [expenseName, setExpenseName] = useState("");
-  const [expenseAmount, setExpenseAmount] = useState("");
+  const [expense, setExpense] = useState({
+    expenseName: "",
+    expenseAmount: 0,
+    paymentMode: "",
+    expenseDate: new Date().toDateString().slice(4),
+  });
+
+  const handleExpenseChange = (fieldName, value) => {
+    setExpense((prevData) => ({
+      ...prevData,
+      [fieldName]: value,
+    }));
+  };
 
   const handleAddExpense = () => {
     setModalVisible(true);
@@ -17,12 +37,15 @@ export default function ExpensesPage() {
 
   const handleSaveExpense = () => {
     // Add your logic to save the expense
-    console.log("Expense Name:", expenseName);
-    console.log("Expense Amount:", expenseAmount);
+    console.log(expense);
 
     // Reset form and close modal
-    setExpenseName("");
-    setExpenseAmount("");
+    setExpense((prevData) => ({
+      ...prevData,
+      expenseName: "",
+      expenseAmount: 0,
+      paymentMode: "",
+    }));
     setModalVisible(false);
   };
 
@@ -32,28 +55,41 @@ export default function ExpensesPage() {
         options={{
           headerShown: true,
           headerTitle: "Expenses",
-          headerTitleStyle: { color: "white", fontFamily: "Nunito", fontSize: 25 },
+          headerTitleStyle: {
+            color: "white",
+            fontFamily: "Nunito",
+            fontSize: 25,
+          },
           headerStyle: { backgroundColor: "#0F0F0F" },
         }}
       />
-      <SafeAreaView>
+      <SafeAreaView className="h-full">
+        <Pressable
+          onPress={handleAddExpense}
+          className="bg-blue-500 p-2 rounded-full absolute right-0 bottom-5 z-10"
+        >
+          <Ionicons name="add" size={40} color="white" />
+        </Pressable>
         <View className="my-4 flex-row justify-between items-center">
           <View>
-            <Text className="text-2xl text-white font-bold">Expenses Summary</Text>
-            <Text className="text-lg text-gray-400">Overview of your monthly expenses</Text>
+            <Text className="text-2xl text-white font-bold">
+              Expenses Summary
+            </Text>
+            <Text className="text-lg text-gray-400">
+              Overview of your monthly expenses
+            </Text>
           </View>
-          <Pressable onPress={handleAddExpense} className="bg-blue-500 p-2 rounded-full">
-            <Ionicons name="add" size={24} color="white" />
-          </Pressable>
         </View>
         <View className="my-4">
           <Text className="text-lg text-gray-400 mb-2">Select Month:</Text>
           <View className="bg-[#1C1C1C] rounded-xl p-2">
             <Picker
               selectedValue={selectedDate}
-              onValueChange={(itemValue, itemIndex) => setSelectedDate(itemValue)}
-              style={{ color: "white", }}
-              itemStyle={{ borderRadius: 50, }}
+              onValueChange={(itemValue, itemIndex) =>
+                setSelectedDate(itemValue)
+              }
+              style={{ color: "white" }}
+              itemStyle={{ borderRadius: 50 }}
             >
               <Picker.Item label="June 2024" value="June 2024" />
               <Picker.Item label="May 2024" value="May 2024" />
@@ -83,16 +119,26 @@ export default function ExpensesPage() {
             <TextInput
               placeholder="Expense Name"
               placeholderTextColor="gray"
-              value={expenseName}
-              onChangeText={setExpenseName}
+              onChangeText={(value) =>
+                handleExpenseChange("expenseName", value)
+              }
               className="bg-[#31363F] text-white p-2 mb-4 rounded-lg"
             />
             <TextInput
               placeholder="Expense Amount"
               placeholderTextColor="gray"
-              value={expenseAmount}
-              onChangeText={setExpenseAmount}
+              onChangeText={(value) =>
+                handleExpenseChange("expenseAmount", Number(value))
+              }
               keyboardType="numeric"
+              className="bg-[#31363F] text-white p-2 mb-4 rounded-lg"
+            />
+            <TextInput
+              placeholder="Payment Mode"
+              placeholderTextColor="gray"
+              onChangeText={(value) =>
+                handleExpenseChange("paymentMode", value)
+              }
               className="bg-[#31363F] text-white p-2 mb-4 rounded-lg"
             />
             <Pressable
