50d52368d79041d609f4b26875bfde956df41178
Arush <55015285+aruahaha@users.noreply.github.com>
2024-09-17 15:08:48 +0530

    pdf download done

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -7,6 +7,7 @@ import {
   Image,
   Modal,
   Pressable,
+  ScrollView,
   TextInput,
   TouchableOpacity,
   View,
@@ -16,19 +17,34 @@ import CustomSuccessAlert from "../../components/modals/CustomSuccessAlert";
 import { supabase } from "../../lib/supabase";
 import { AntDesign } from "@expo/vector-icons";
 import { useUser } from "../../components/globalState/UserContext";
+import { printToFileAsync } from "expo-print";
+import { shareAsync } from "expo-sharing";
+import { numberWithCommas } from "../utils";
+import AlertScreen from "../../screens/AlertScreen";
+import CustomAlert from "../../components/modals/CustomAlert";
 
 const ProfilePage = () => {
   const { colors } = useTheme();
   const { user } = useUser()
-  const isFocused = useIsFocused();
-  const [profileModal, setProfileModal] = useState(false);
-  const [confirmModal, setConfirmModal] = useState(false);
   const [loading, setLoading] = useState(true);
   const [userName, setUserName] = useState("");
   const [oldIncome, setOldIncome] = useState(0);
   const [income, setIncome] = useState(0);
-  const [logOutModal, setLogOutModal] = useState(false);
+  const [savings, setSavings] = useState(0);
+  const [expenses, setExpenses] = useState([]);
   const [alertVisible, setAlertVisible] = useState(false);
+  const [showModal, setShowModal] = useState()
+  const [selectedMY, setSelectedMY] = useState({
+    month: "",
+    year: ""
+  })
+
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: 5 }, (_, i) => currentYear + i);
+
+  const months = Array.from({ length: 12 }, (_, i) =>
+    new Date(-1, i).toLocaleString("default", { month: "long" })
+  );
 
   const handleChangeSubmit = async () => {
     const incomeNumber = Number(income);
@@ -52,10 +68,9 @@ const ProfilePage = () => {
         if (updateError) {
           Alert.alert("Error updating profile", updateError.message);
         } else {
-          setAlertVisible(true);
+          setAlertVisible("profileUpdated");
         }
-        setProfileModal(false);
-        setConfirmModal(false);
+        setShowModal(null);
         setLoading(true);
       }
     } else {
@@ -73,7 +88,7 @@ const ProfilePage = () => {
         } else {
           setAlertVisible(true);
         }
-        setProfileModal(false);
+        setShowModal(null);
         setLoading(true);
       }
     }
@@ -88,7 +103,7 @@ const ProfilePage = () => {
   async function fetchData() {
     const { data, error } = await supabase
       .from("User Data")
-      .select("username,income")
+      .select("username,income,expenses,savings")
       .eq("email", user?.user_metadata?.email);
 
     if (error) {
@@ -98,6 +113,8 @@ const ProfilePage = () => {
       setUserName(data[0]?.username || "");
       setIncome(data[0]?.income || 0);
       setOldIncome(data[0]?.income || 0);
+      setExpenses(data[0]?.expenses || [])
+      setSavings(data[0]?.savings || 0);
     }
   }
 
@@ -109,6 +126,92 @@ const ProfilePage = () => {
       setLoading(false);
     }, 2000);
   }, [user]);
+
+  const filterdExpensePDF = expenses?.filter((expense) => (
+    expense.expenseDate.slice(0, 3) === selectedMY.month.slice(0, 3) && Number(expense.expenseDate.slice(7, 11)) === selectedMY.year
+  ))
+
+  const html = `
+      <html>
+    <head>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+            }
+            h1 {
+                color: #41B3A2;
+            }
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+            th, td {
+                border: 1px solid #dddddd;
+                text-align: left;
+                padding: 8px;
+            }
+            th {
+                background-color: #41B3A2;
+                color: white;
+            }
+            .total-row {
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>${selectedMY.month} ${selectedMY.year} Expenses</h1>
+        <table>
+            <tr>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Category</th>
+            </tr>
+            ${filterdExpensePDF.map(expense => `
+                <tr>
+                    <td>${expense.expenseDate}</td>
+                    <td>${expense.expenseAmount}</td>
+                    <td>${expense.expenseName}</td>
+                </tr>
+            `).join('')}
+            <tr class="total-row">
+                <td colspan="1">Total Expense</td>
+                <td>${numberWithCommas(filterdExpensePDF?.reduce((sum, expense) => sum + expense.expenseAmount, 0))}</td>
+                <td></td>
+            </tr>
+            <tr class="total-row">
+                <td colspan="1">Savings</td>
+                <td>${income - (filterdExpensePDF?.reduce((sum, expense) => sum + expense.expenseAmount, 0))}</td>
+                <td></td>
+            </tr>
+        </table>
+    </body>
+</html>
+
+    `
+
+  const alertConfig = {
+    noDataPdf: {
+      mainMessage: "No Data",
+      message: `There is no data on ${selectedMY.month} ${selectedMY.year}`,
+      alerts: true,
+      AlertScreen: AlertScreen,
+    },
+  };
+
+  let generatePdf = async () => {
+    if (filterdExpensePDF.length > 0) {
+
+      const file = await printToFileAsync({
+        html: html,
+        base64: false
+      });
+
+      await shareAsync(file.uri)
+    } else {
+      setAlertVisible("noDataPdf")
+    }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: colors.background }}>
@@ -130,10 +233,6 @@ const ProfilePage = () => {
 
       <View className="px-6">
         {loading ? (
-          // <View className="items-center mt-8">
-          //   <Skeleton h="3" w="120" rounded="full" marginY="5" />
-          //   <Skeleton h="3" rounded="full" />
-          // </View>
           <View className="items-center mt-8">
             <ActivityIndicator size={20} color={colors.text} />
           </View>
@@ -156,7 +255,7 @@ const ProfilePage = () => {
             className="p-4 rounded-full flex-row justify-between items-center shadow-md"
             style={{ backgroundColor: colors.inputBg }}
             onPress={() => {
-              setProfileModal(true);
+              setShowModal("profileModal");
               fetchData();
             }}
           >
@@ -175,9 +274,25 @@ const ProfilePage = () => {
             </CustomText>
           </Link>
 
+          {/* <TouchableOpacity
+            className="p-4 rounded-full flex-row justify-between items-center mt-4 shadow-md"
+            style={{ backgroundColor: colors.inputBg }}
+            onPress={generatePdf}
+          >
+            <CustomText className="text-lg text-white ">Export PDF</CustomText>
+          </TouchableOpacity> */}
+
+          <TouchableOpacity
+            className="p-4 rounded-full flex-row justify-between items-center mt-4 shadow-md"
+            style={{ backgroundColor: colors.inputBg }}
+            onPress={() => setShowModal("datePickerModal")}
+          >
+            <CustomText className="text-lg text-white ">Export PDF</CustomText>
+          </TouchableOpacity>
+
           <TouchableOpacity
             className="p-4 rounded-full flex-row justify-between items-center mt-4 bg-red-500 shadow-md"
-            onPress={() => setLogOutModal(true)}
+            onPress={() => setShowModal("logOut")}
           >
             <CustomText className="text-lg text-white ">Log Out</CustomText>
           </TouchableOpacity>
@@ -187,9 +302,9 @@ const ProfilePage = () => {
       <Modal
         animationType="slide"
         transparent={true}
-        visible={profileModal}
+        visible={showModal === "profileModal"}
         onRequestClose={() => {
-          setProfileModal(!profileModal);
+          setShowModal(null);
         }}
       >
         <View
@@ -227,7 +342,7 @@ const ProfilePage = () => {
               className="p-4 bg-[#41B3A2] items-center rounded-full mt-6 shadow-md"
               onPress={() => {
                 if (oldIncome !== income) {
-                  setConfirmModal(true);
+                  setShowModal("confirmModal");
                 } else {
                   handleChangeSubmit();
                 }
@@ -241,7 +356,7 @@ const ProfilePage = () => {
             </Pressable>
             <Pressable
               className="p-4 bg-red-500 items-center rounded-full mt-4 shadow-md"
-              onPress={() => setProfileModal(false)}
+              onPress={() => setShowModal(null)}
             >
               <CustomText className="text-white text-lg">Cancel</CustomText>
             </Pressable>
@@ -252,9 +367,9 @@ const ProfilePage = () => {
       <Modal
         animationType="slide"
         transparent={true}
-        visible={confirmModal}
+        visible={showModal === "confirmModal"}
         onRequestClose={() => {
-          setConfirmModal(!confirmModal);
+          setShowModal(null);
         }}
       >
         <View
@@ -281,7 +396,7 @@ const ProfilePage = () => {
               <Pressable
                 className="flex-1 p-4 bg-red-500 items-center rounded-full shadow-md"
                 onPress={() => {
-                  setConfirmModal(false);
+                  setShowModal(null);
                 }}
               >
                 <CustomText className="text-white text-lg">No</CustomText>
@@ -300,9 +415,116 @@ const ProfilePage = () => {
       <Modal
         animationType="slide"
         transparent={true}
-        visible={logOutModal}
+        visible={showModal === "datePickerModal"}
         onRequestClose={() => {
-          setLogOutModal(!logOutModal);
+          setShowModal(null);
+        }}
+      >
+        <View
+          className="flex-1 justify-center items-center bg-opacity-80"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
+        >
+          <View
+            className="rounded-3xl p-4 w-10/12 max-h-1/2"
+            style={{ backgroundColor: colors.expenseForm }}
+          >
+            <CustomText
+              className="text-xl text-center mb-4"
+              style={{ color: colors.text }}
+            >
+              Select Month & Year
+            </CustomText>
+            <View className="flex-row justify-between mb-4">
+              <View className=" gap-x-2 w-full flex-row">
+                <ScrollView
+                  style={{ maxHeight: 300 }}
+                  nestedScrollEnabled={true}
+                >
+                  {months.map((item, i) => (
+                    <View
+                      key={i}
+                      className="my-2 py-4 px-4 rounded-3xl"
+                      style={{
+                        backgroundColor: selectedMY.month === item ? "#41B3A2" : colors.expenseInput,
+                        color: colors.text
+                      }}
+                    >
+                      <Pressable
+                        onPress={() => {
+                          setSelectedMY({
+                            ...selectedMY,
+                            month: item
+                          })
+                        }}
+                      >
+                        <CustomText
+                          className="text-lg"
+                          style={{
+                            backgroundColor: selectedMY.month === item ? "#41B3A2" : colors.expenseInput,
+                            color: colors.text
+                          }}
+                        >
+                          {item}
+                        </CustomText>
+                      </Pressable>
+                    </View>
+                  ))}
+                </ScrollView>
+                <ScrollView
+                  style={{ maxHeight: 300 }}
+                  nestedScrollEnabled={true}
+                >
+                  {years.map((item, i) => (
+                    <View
+                      key={i}
+                      className="my-2 py-4 px-4 rounded-3xl"
+                      style={{
+                        backgroundColor: selectedMY.year === item ? "#41B3A2" : colors.expenseInput,
+                        color: colors.text
+                      }}
+                    >
+                      <Pressable
+                        onPress={() => {
+                          setSelectedMY({
+                            ...selectedMY,
+                            year: item
+                          })
+                        }}
+                      >
+                        <CustomText
+                          className="text-lg"
+                          style={{
+                            backgroundColor: selectedMY.year === item ? "#41B3A2" : colors.expenseInput,
+                            color: colors.text
+                          }}
+                        >
+                          {item}
+                        </CustomText>
+                      </Pressable>
+                    </View>
+                  ))}
+                </ScrollView>
+              </View>
+            </View>
+            <Pressable
+              className="bg-[#41B3A2] rounded-3xl p-4 items-center mt-3"
+              onPress={() => {
+                setShowModal(null)
+                generatePdf()
+              }}
+            >
+              <CustomText className="text-white">Confirm</CustomText>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal === "logOut"}
+        onRequestClose={() => {
+          setShowModal(null);
         }}
       >
         <View
@@ -322,7 +544,7 @@ const ProfilePage = () => {
             <View className="flex-row gap-3">
               <Pressable
                 className="flex-1 p-4 bg-red-500 items-center rounded-full shadow-md"
-                onPress={() => setLogOutModal(false)}
+                onPress={() => setShowModal(null)}
               >
                 <CustomText className="text-white text-lg">No</CustomText>
               </Pressable>
@@ -338,11 +560,24 @@ const ProfilePage = () => {
       </Modal>
       <View className="flex-1">
         <CustomSuccessAlert
-          visible={alertVisible}
+          visible={alertVisible === "profileUpdated"}
           mainMessage="Profile Updated"
           message="Your profile updated successfully!"
-          onClose={() => setAlertVisible(false)}
+          onClose={() => setAlertVisible(null)}
         />
+      </View>
+      <View className="flex-1">
+        {alertVisible && (
+          <CustomAlert
+            visible={!!alertVisible}
+            mainMessage={alertConfig[alertVisible]?.mainMessage}
+            message={alertConfig[alertVisible]?.message}
+            onClose={() => setAlertVisible(null)}
+            alerts={alertConfig[alertVisible]?.alerts}
+            task={alertConfig[alertVisible]?.task}
+            AlertScreen={alertConfig[alertVisible]?.AlertScreen}
+          />
+        )}
       </View>
     </View>
   );
